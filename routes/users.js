a7a08b8444e3daa43d2835ff776d9464dac3106e
ArkadiyGalystov <119405763+ArkadiyGalystov@users.noreply.github.com>
2023-08-22 18:40:04 +0300

    Update users.js
    исправление валидации

--- routes/users.js
+++ routes/users.js
@@ -1,19 +1,15 @@
 const router = require('express').Router();
 const { celebrate, Joi } = require('celebrate');
 
-const {
-  getCurrentUserId,
-  updateUser,
-} = require('../controllers/users');
+const { getCurrentUserId, updateUser } = require('../controllers/users');
 
 router.get('/me', getCurrentUserId);
 
-router.patch(
-  '/me',
+router.patch('/me',
   celebrate({
     body: Joi.object().keys({
-      name: Joi.string().min(2).max(30).required(),
-      about: Joi.string().min(2).max(30).required(),
+      name: Joi.string().min(2).max(30),
+      email: Joi.string().email(),
     }),
   }),
   updateUser,
