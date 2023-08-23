d34e221328ceef8f40df76b4a8437244e028ecec
ArkadiyGalystov <119405763+ArkadiyGalystov@users.noreply.github.com>
2023-08-23 17:55:36 +0300

    исправление 2

--- utils/constants.js
+++ utils/constants.js
@@ -3,6 +3,7 @@ const { config } = require('dotenv');
 const URL = 'mongodb://127.0.0.1:27017/bitfilmsdb';
 const REGEX_URL = /https?:\/\/(?:[-\w]+\.)?([-\w]+)\.\w+(?:\.\w+)?\/?.*/;
 const REGEX_URL_EMAIL = /^[A-Z0-9._%+-]+@[A-Z0-9-]+.+.[A-Z]{2,4}$/i;
+const REG_EN = /^[a-zA-Z\s-]+$/;
 const { SECRET_KEY = 'dev-secret' } = process.env;
 const { PORT = 3000 } = process.env;
 const { NODE_ENV } = process.env;
@@ -18,4 +19,5 @@ module.exports = {
   REGEX_URL,
   REGEX_URL_EMAIL,
   NODE_ENV,
+  REG_EN,
 };
